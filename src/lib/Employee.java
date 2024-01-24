9e3377d345130241801952d5f7587252118eb32a
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-01-24 11:28:09 +0300

    Added object constructors for class Employee

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -31,4 +31,37 @@ public class Employee {
      */
     Scanner news = new Scanner(System.in);
     
+    /**
+     * Constructs an Employee object with the specified parameters.
+     *
+     * @param name     The name of the employee.
+     * @param gender   The gender of the employee.
+     * @param dept     The department in which the employee works.
+     * @param yob      The year of birth of the employee.
+     * @param fnumber  The file number associated with the employee.
+     * @param salary   The salary of the employee.
+     */
+    public Employee(String name, String gender, String dept, int yob, int fnumber, double salary){
+        this.name = name;
+        this.gender = gender;
+        this.dept = dept;
+        this.yob = yob;
+        this.fnumber = fnumber;
+        this.salary = salary;
+    }
+    
+    /**
+     * Default constructor for creating an Employee object by taking user input.
+     * Prompts the user to enter name, gender, department, year of birth, file number, and salary.
+     */
+    public Employee(){
+        System.out.println("Enter The Name, Gender, Department, Year Of Birth, File Number & Salary Of The Employee");
+        name = news.next();
+        gender = news.next();
+        dept = news.next();
+        yob = news.nextInt();
+        fnumber = news.nextInt();
+        salary = news.nextDouble();
+    }
+    
 }
