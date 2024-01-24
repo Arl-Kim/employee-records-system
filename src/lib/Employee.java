f463af4f43eda30faec831b0b3f1b05570a3fa7c
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-01-24 10:47:35 +0300

    Declared the Fields to be used in class Employee.
    They were not declared in the Initial Commit (Check Initial Commit Message) due to an issue with IDE's autosave feature.

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -5,9 +5,25 @@
 package lib;
 
 /**
+ *The Employee class represents an individual employee with basic information
+ * such as name, gender, department, year of birth, file number, and salary.
+ * It provides methods to retrieve salary details and general employee information.
  *
+ * This class also includes constructors for initializing the instance variables
+ * and default constructor for taking input from the user.
+ * 
+ * 
  * @author KIMUTAI
  */
+
+import java.util.Scanner;
+
 public class Employee {
-    
+    protected String name;
+    protected String gender;
+    protected String dept;
+    protected int yob;
+    protected int fnumber;
+    protected double salary;
+
 }
